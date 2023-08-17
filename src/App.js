9e9a9b7bcd4bd0e5d2a8aa7068ccd0d1b4d379b9
neeleshk143 <44637226+neeleshk143@users.noreply.github.com>
2023-08-17 17:18:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,33 +10,46 @@ const cardsData = [
     title: 'Track Your Conversion',
     description: 'We use the most progressive tracking and analytics technology to ensure that every call can be tracked and evaluated',
   },
-  {
-    title: 'Customized Campaigns',
-    description: 'Audiences, budgets, and goals: you decide the criteria for your digital ad campaign, and we’ll do the rest.',
+];
+const cardsDataa = [
+  {    title: 'Customized Campaigns',
+  description: 'Audiences, budgets, and goals: you decide the criteria for your digital ad campaign, and we’ll do the rest.',
+  
+  },
+  {    title: 'Get Dedicated Support Team',
+  description: 'We constantly monitor quality control! Our affiliate network of publishers are digital ad experts that have been vetted.',
+  
   },
   {
-    title: ' Get Dedicated Support Team',
-    description: 'We constantly monitor quality control! Our affiliate network of publishers are digital ad experts that have been vetted.',
-  },
-  {
-    title: 'Quality  Assurance',
+    title: 'Quality Assurance',
     description: 'We constantly monitor quality control! Our affiliate network of publishers are digital ad experts that have been vetted.',
   },
 ];
 
 function App() {
   return (
-    <div class="choose-container">
-  <div class="choose-left">
-    <div class="title-container chooseus-title">
-      <h3>Why Choose Us?</h3>
-      <h2>Why the industry chooses Clickdee?</h2>
+    <div className="choose-container">
+  <div className="choose-left">
+    <div className="title-container chooseus-title">
+      <h3 className='title'>Why Choose Us?</h3>
+      <h2 className='subtitle'>Why the industry chooses Clickdee?</h2>
     </div>
     <p>We understand performance marketing from every angle and every stage of the funnel. Our clients trust that we know what metrics move their business towards growth. Our publisher and affiliate partners know that we make maximum revenue and ROAS a main focus when growing our partnerships.</p>
   </div>
   <div class="choose-right">
     <div className="container">
       {cardsData.map((card, index) => (
+        <div key={index} className="card">
+          <div className="card-content">
+            <div className="card-title">{card.title}</div>
+            <div className="description">{card.description}</div>
+          </div>
+          
+        </div>
+      ))}
+      </div>
+    <div className="container">
+      {cardsDataa.map((card, index) => (
         <div key={index} className="card">
           <div className="card-content">
             <div className="card-title">{card.title}</div>
