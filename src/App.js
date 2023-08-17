0f6b8e8dfab701425d4b60a105abb7a35cfe29ea
neeleshk143 <aneelesh342@gmail.com>
2023-08-17 16:24:25 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const cardsData = [
+  {    title: 'Choose Your Local Targeting',
+  description: 'Our targeted and tracked calls are tailored to your business needs, audience, and geolocation.',
+  
+  },
+  {
+    title: 'Track Your Conversion',
+    description: 'We use the most progressive tracking and analytics technology to ensure that every call can be tracked and evaluated',
+  },
+  {
+    title: 'Customized Campaigns',
+    description: 'Audiences, budgets, and goals: you decide the criteria for your digital ad campaign, and we’ll do the rest.',
+  },
+  {
+    title: ' Get Dedicated Support Team',
+    description: 'We constantly monitor quality control! Our affiliate network of publishers are digital ad experts that have been vetted.',
+  },
+  {
+    title: 'Quality  Assurance',
+    description: 'We constantly monitor quality control! Our affiliate network of publishers are digital ad experts that have been vetted.',
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="choose-container">
+  <div class="choose-left">
+    <div class="title-container chooseus-title">
+      <h3>Why Choose Us?</h3>
+      <h2>Why the industry chooses Clickdee?</h2>
+    </div>
+    <p>We understand performance marketing from every angle and every stage of the funnel. Our clients trust that we know what metrics move their business towards growth. Our publisher and affiliate partners know that we make maximum revenue and ROAS a main focus when growing our partnerships.</p>
+  </div>
+  <div class="choose-right">
+    <div className="container">
+      {cardsData.map((card, index) => (
+        <div key={index} className="card">
+          <div className="card-content">
+            <div className="card-title">{card.title}</div>
+            <div className="description">{card.description}</div>
+          </div>
+          
+        </div>
+      ))}
+      </div>
+    </div>
     </div>
   );
 }
